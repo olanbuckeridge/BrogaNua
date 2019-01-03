7d7186871d58df4bf00c52a226b1dc474975c883
Olan Buckeridge <olan.buckeridge2@mail.dcu.ie>
2019-01-03 14:30:54 +0000

    Fixed activity switch.

--- src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/NewsFragment.java
+++ src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/NewsFragment.java
@@ -13,6 +13,6 @@ public class NewsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_catalog, container, false);
+        return inflater.inflate(R.layout.fragment_news, container, false);
     }
 }
