e266ca28e55339773fa17b19f087b1b2a18cde36
Olan Buckeridge <olan.buckeridge2@mail.dcu.ie>
2019-02-01 12:17:08 +0000

    Added layout to Profile Fragment.

--- src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/ProfileFragment.java
+++ src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/ProfileFragment.java
@@ -4,15 +4,35 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView userName, userAge, userEmail, userShoeSize;
+    private Button userEditButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        userName = (TextView)view.findViewById(R.id.tvProfileName);
+        userAge = (TextView)view.findViewById(R.id.tvProfileAge);
+        userEmail = (TextView)view.findViewById(R.id.tvProfileEmail);
+        userShoeSize = (TextView)view.findViewById(R.id.tvProfileShoeSize);
+        userEditButton = (Button)view.findViewById(R.id.buttonProfileUpdate);
+        Log.d(TAG, "onCreateView: started.");
+
+
+        return view;
     }
 }
