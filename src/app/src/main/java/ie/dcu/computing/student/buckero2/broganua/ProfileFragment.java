4a9c5b3173936dbca096081c4459227a71cb00dc
Olan Buckeridge <olan.buckeridge2@mail.dcu.ie>
2019-02-01 12:42:02 +0000

    Added Database info to Profile page.

--- src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/ProfileFragment.java
+++ src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/ProfileFragment.java
@@ -10,6 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
@@ -17,20 +25,43 @@ import static android.support.constraint.Constraints.TAG;
 
 public class ProfileFragment extends Fragment {
 
-    private TextView userName, userAge, userEmail, userShoeSize;
-    private Button userEditButton;
+    private TextView profileName, profileAge, profileEmail, profileShoeSize;
+    private Button profileEditButton;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        userName = (TextView)view.findViewById(R.id.tvProfileName);
-        userAge = (TextView)view.findViewById(R.id.tvProfileAge);
-        userEmail = (TextView)view.findViewById(R.id.tvProfileEmail);
-        userShoeSize = (TextView)view.findViewById(R.id.tvProfileShoeSize);
-        userEditButton = (Button)view.findViewById(R.id.buttonProfileUpdate);
+        profileName = (TextView)view.findViewById(R.id.tvProfileName);
+        profileAge = (TextView)view.findViewById(R.id.tvProfileAge);
+        profileEmail = (TextView)view.findViewById(R.id.tvProfileEmail);
+        profileShoeSize = (TextView)view.findViewById(R.id.tvProfileShoeSize);
+        profileEditButton = (Button)view.findViewById(R.id.buttonProfileUpdate);
         Log.d(TAG, "onCreateView: started.");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                profileName.setText("Name: " + userProfile.getUserName());
+                profileAge.setText("Age: " + userProfile.getUserAge());
+                profileEmail.setText("Email: " + userProfile.getUserEmail());
+                profileShoeSize.setText("Shoe Size (EU): " + userProfile.getUserShoeSize());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         return view;
--- src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/UserProfile.java
+++ src/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/UserProfile.java
@@ -3,15 +3,51 @@ package ie.dcu.computing.student.buckero2.broganua;
 import java.lang.reflect.Constructor;
 
 public class UserProfile {
-    public String Age;
-    public String Email;
-    public String Name;
-    public String ShoeSize;
+    public String userAge;
+    public String userEmail;
+    public String userName;
+    public String userShoeSize;
+
+    public UserProfile() {
+
+    }
 
     public UserProfile(String userAge, String userEmail, String userName, String userShoeSize) {
-        this.Age = userAge;
-        this.Email = userEmail;
-        this.Name = userName;
-        this.ShoeSize = userShoeSize;
+        this.userAge = userAge;
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.userShoeSize = userShoeSize;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserShoeSize() {
+        return userShoeSize;
+    }
+
+    public void setUserShoeSize(String userShoeSize) {
+        this.userShoeSize = userShoeSize;
     }
 }
