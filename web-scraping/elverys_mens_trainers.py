44bf58046a14e778829191dd818ef7b4ac1bf9f5
Olan Buckeridge <olan.buckeridge2@mail.dcu.ie>
2019-03-26 17:51:47 +0000

    Refined Elverys data.

--- web-scraping/elverys_mens_trainers.py
+++ web-scraping/elverys_mens_trainers.py
@@ -3,31 +3,43 @@ from bs4 import BeautifulSoup as soup
 import re
 import pymysql
 
-'''conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='r9mtj6ta', db='products')
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='r9mtj6ta', db='products')
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM products")'''
+cur.execute("SELECT * FROM products")
 
 # Opening connection, grabbing the page
-items = 0
-while items < 6:
-    uClient = uReq("https://www.elverys.ie/elverys/en/Elverys/Men/c/2296?q=%3Arelevance%3AcategoryCode%3A2514%3AtypeCode%3A2516&page={}".format(items))
-    page_html = uClient.read()
-    uClient.close()
-    page_soup = soup(page_html, "html.parser")
+uClient = uReq("https://www.elverys.ie/elverys/en/Elverys/Men/c/2296?q=%3Arelevance%3AcategoryCode%3A2514%3AtypeCode%3A2516&show=All")
+page_html = uClient.read()
+uClient.close()
+page_soup = soup(page_html, "html.parser")
 
-    containers = page_soup.findAll("div", {"class":"product-box grid-prdt"})
-    #image_containers = page_soup.findAll("img")
-    #for image_tag in image_containers:
-    #    print (image_tag['src'])
-    #img_url = image_containers.get("src")
-    for container in containers:
-        brand_container = container.findAll("h4")
-        brand = brand_container[0].text.strip()
-        print ("Brand:",brand)
-        model_container = container.findAll("a",{"class":"prdt-name"})
-        model = model_container[0].text.strip()
-        print (model)
+containers = page_soup.findAll("div", {"class":"col-sm-3 loadelement"})
 
-    #print (containers)
-    items += 1
+brands = ['Nike','Puma','Converse','adidas','New Balance','Timberland','Skechers']
+for container in containers:
+    brand_container = container.findAll("h4")
+    brand = brand_container[0].text.strip()
+
+    model_container = container.findAll("a",{"class":"prdt-name"})
+    model = model_container[0].text.strip()
+    for i in range(len(brands)):
+        if brands[i] in model:
+            model = model.split(brands[i],1)[1]
+            model = model.split(",",1)[0]
+            model = model.split("Men",1)[0]
+
+    price_container = container.findAll("div",{"class":"prdt-price"})
+    sale_container = price_container[0].find("span")
+    if sale_container != None:
+        price = sale_container.text.strip()
+    else:
+        price = price_container[0].text.strip()
+
+    image_container = container.findAll("a",{"class":"prdt-img"})
+    image = image_container[0].find("img")
+    img_url = "https://www.elverys.ie" + image["src"]
+    retailer = 'Elverys'
+
+    cur.execute("INSERT INTO products (brand, model, retailer, price, images) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\")", (brand, model, retailer, price.strip("€"), img_url))
+    cur.connection.commit()
