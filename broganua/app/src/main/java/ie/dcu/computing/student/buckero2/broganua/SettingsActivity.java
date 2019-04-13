db79841b9894c6e3e1f813bf43c21141acc8fe7d
Olan Buckeridge <olan.buckeridge2@mail.dcu.ie>
2019-04-13 12:28:17 +0100

    Improving user settings.

--- broganua/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/SettingsActivity.java
+++ broganua/app/src/main/java/ie/dcu/computing/student/buckero2/broganua/SettingsActivity.java
@@ -1,8 +1,13 @@
 package ie.dcu.computing.student.buckero2.broganua;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 
 public class SettingsActivity extends PreferenceActivity {
@@ -21,6 +26,37 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            bindSummaryValue(findPreference("key_full_name"));
+            bindSummaryValue(findPreference("key_email"));
+            //bindSummaryValue(findPreference("key_sportswear_brands"));
+            //bindSummaryValue(findPreference("key_luxury_brands"));
+
         }
     }
+
+    private static void bindSummaryValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(listener);
+        listener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                .getString(preference.getKey(),""));
+    }
+
+    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            if (preference instanceof MultiSelectListPreference) {
+                MultiSelectListPreference multiSelectListPreference = (MultiSelectListPreference) preference;
+                int index = multiSelectListPreference.findIndexOfValue(stringValue);
+                // Set the summary to reflect the new value
+                preference.setSummary(index >= 0
+                        ? multiSelectListPreference.getEntries()[index]
+                        : null);
+            } else if (preference instanceof EditTextPreference) {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
 }
