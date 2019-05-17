cbeec66ba7280f870fcaed33118fc14fbdf0152e
Olan Buckeridge <olan.buckeridge2@mail.dcu.ie>
2019-05-17 17:56:34 +0100

    Refactoring source code.

--- src/android-application/app/src/test/java/ie/dcu/computing/student/buckero2/broganua/ExampleUnitTest.java
+++ src/android-application/app/src/test/java/ie/dcu/computing/student/buckero2/broganua/ExampleUnitTest.java
@@ -1,6 +1,12 @@
 package ie.dcu.computing.student.buckero2.broganua;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.Shadows;
 
 import static org.junit.Assert.*;
 
@@ -8,10 +14,32 @@ import static org.junit.Assert.*;
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+} */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class LoginUnitTest {
+    // test code goes here
+    LoginActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(LoginActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception {
+        assertNotNull(activity);
+    }
 }
+
+
